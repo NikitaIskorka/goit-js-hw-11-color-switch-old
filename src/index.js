d3c1fdb225bc75345f1f11822667d134fcdd68e2
NikitaIskorka <67067414+NikitaIskorka@users.noreply.github.com>
2020-12-10 12:58:11 +0100

    change-color-done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,85 @@
 import './styles.css';
+const colors = [
+  '#FF6633',
+  '#FFB399',
+  '#FF33FF',
+  '#FFFF99',
+  '#00B3E6',
+  '#E6B333',
+  '#3366E6',
+  '#999966',
+  '#99FF99',
+  '#B34D4D',
+  '#80B300',
+  '#809900',
+  '#E6B3B3',
+  '#6680B3',
+  '#66991A',
+  '#FF99E6',
+  '#CCFF1A',
+  '#FF1A66',
+  '#E6331A',
+  '#33FFCC',
+  '#66994D',
+  '#B366CC',
+  '#4D8000',
+  '#B33300',
+  '#CC80CC',
+  '#66664D',
+  '#991AFF',
+  '#E666FF',
+  '#4DB3FF',
+  '#1AB399',
+  '#E666B3',
+  '#33991A',
+  '#CC9999',
+  '#B3B31A',
+  '#00E680',
+  '#4D8066',
+  '#809980',
+  '#E6FF80',
+  '#1AFF33',
+  '#999933',
+  '#FF3380',
+  '#CCCC00',
+  '#66E64D',
+  '#4D80CC',
+  '#9900B3',
+  '#E64D66',
+  '#4DB380',
+  '#FF4D4D',
+  '#99E6E6',
+  '#6666FF',
+];
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector("button[data-action='start']"),
+  stopBtn: document.querySelector("button[data-action='stop']"),
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', startBtnChangeColor);
+refs.stopBtn.addEventListener('click', stopBtnOffChangeColor);
+
+function startBtnChangeColor() {
+  if (refs.startBtn.classList.contains('active')) {
+    return;
+  }
+  refs.startBtn.classList.add('active');
+  intervalId = setInterval(() => {
+    (refs.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)]),
+      1000;
+  });
+}
+
+function stopBtnOffChangeColor() {
+  refs.startBtn.classList.remove('active');
+  clearInterval(intervalId);
+  refs.body.style.backgroundColor = '#FFFF';
+}
